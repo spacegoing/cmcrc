6bdd9bf7089d251eb2ec4beb51cd14d786ea4ef5
spacegoing <spacegoing@8mxfzf2.cmcrc.com>
2018-04-24 23:29:09 +1000

    [New] clear list

--- mqd_training/ass/official_list/script.py
+++ mqd_training/ass/official_list/script.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import six
 
 ## filter PDF->Excel file; select Equi from listed securities
@@ -6,30 +7,70 @@ def filter(filename):
     bdt_df = pd.read_excel(filename, 1, header=None, index_col=None)
     bdt_df_mat = bdt_df.as_matrix()
     bdt_equi_list = []
+    bdt_gdrs_list = []
     for d in bdt_df_mat:
-        skip = False
+        equi_flag = False
+        gdrs_flag = False
         for s in d:
             if isinstance(s, six.string_types):
-                # if "GDR" in s:
-                #     break
-                if "Equi." in s and skip == False:
-                    bdt_equi_list.append(d)
+                if "equi." in s.lower():
+                    equi_flag = True
+                if ("gdr" in s.lower()) or ("gds" in s.lower()) :
+                    gdrs_flag = True
 
-    return bdt_equi_list
+        if equi_flag:
+            if gdrs_flag:
+                bdt_gdrs_list.append(d)
+            else:
+                bdt_equi_list.append(d)
+    return pd.DataFrame(bdt_equi_list), pd.DataFrame(bdt_gdrs_list)
 
-bdt_equi_list = filter('Bdt.xlsx')
-mf_equi_list = filter('EuroMF.xlsx')
-total = bdt_equi_list + mf_equi_list
+bdt_equi_list, bdt_gdrs_list = filter('Bdt.xlsx')
+mf_equi_list, mf_gdrs_list = filter('EuroMF.xlsx')
+# bdt_equi_list.to_csv('bdt_equi_list.csv')
+# bdt_gdrs_list.to_csv('bdt_gdrs_list.csv')
+# mf_equi_list.to_csv('mf_equi_list.csv')
+# mf_gdrs_list.to_csv('mf_gdrs_list.csv')
 
-df_total = pd.DataFrame(total)
-df_total.to_csv('filtered_list.csv')
+## reformat: merge multiple name columns into one
+def reformat(raw_mat):
+    raw_mat = raw_mat.as_matrix()
+    ref_mat = np.empty((raw_mat.shape[0],7),dtype=np.object)
+    for i,r in enumerate(raw_mat):
+        ref_mat[i,0] = r[0]
+        name_str = ''
+        for j,c in enumerate(r[1:]):
+            if isinstance(c, six.string_types):
+                if "equi." in c.lower():
+                    break
+                else:
+                    name_str += " " + c
+                    ref_mat[i,1] = name_str
 
-##
+        counter = 0
+        for s in r[j+1:]:
+            if isinstance(s, datetime.datetime):
+                s = s.strftime('%d/%m/%Y')
+            if isinstance(s, six.string_types):
+                ref_mat[i,2+counter] = s
+                counter+=1
 
-filename = 'raw_rics.csv'
-bdt_df = pd.read_excel(filename, 1, header=None, index_col=None)
-bdt_df_mat = bdt_df.as_matrix()
+    return pd.DataFrame(ref_mat)
 
+mat_list = [bdt_equi_list, bdt_gdrs_list, mf_equi_list, mf_gdrs_list]
+reformated_list =[reformat(i) for i in mat_list]
+filename_str = ['Bdt_equi_list.csv', 'Bdt_gdrs_list.csv', 'EuroMF_equi_list.csv','EuroMF_gdrs_list.csv']
 
+[i.to_csv("equity_filtered_list/"+j) for i,j in zip(reformated_list, filename_str)]
 
+## compare trth & filtered result
+trth_list = pd.read_csv('trth_equity_speedguide.csv', header=None, index_col=None).as_matrix()[1:,:]
+isin_list = trth_list[:,1].tolist()
+official_list = reformated_list[0].as_matrix()[:,0].tolist() + reformated_list[2].as_matrix()[:,0].tolist()
 
+isin_list = [i.split("->")[-1] if "->" in i else i for i in isin_list]
+
+wrong_isin=list()
+
+not_in_official = set(isin_list) - set(official_list)
+not_in_trth = set(official_list) - set(isin_list)
